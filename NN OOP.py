7798c32c6a1fd3778fa04af146474439465ac9e1
OmarML <omar33@live.co.uk>
2016-09-03 00:23:42 +0100

    Update NN OOP.py

--- NN OOP.py
+++ NN OOP.py
@@ -10,9 +10,13 @@ class NeuralNetwork(object):
         self.HiddenLayers = HiddenLayers
         self.HiddenUnits = HiddenUnits
         self.weights = None
+        self.z = None
 
     def Logistic(self, z):
         return 1 / (1 + np.exp(-z))
+
+    def LogisticPrime(self, z):
+        return np.exp(-z) / ((1 + np.exp(-z))**2)
 
     def CreateWeights(self):
         w1 = np.random.rand(self.InputUnits, self.HiddenUnits)
@@ -28,10 +32,39 @@ class NeuralNetwork(object):
         new_X = self.Logistic(np.dot(X, self.weights[0]))
         for i in self.weights[1:]:
             new_X = np.dot(new_X, i)
-            self.Logistic(new_X)
+            new_X = self.Logistic(new_X)
         return new_X
+
+    def GetZValues(self, X):
+        new_X = self.Logistic(np.dot(X, self.weights[0]))
+        self.z = []
+        self.z.append(np.dot(X, self.weights[0]))
+        for i in self.weights[1:]:
+            new_X = np.dot(new_X, i)
+            self.z.append(new_X)
+            new_X = self.Logistic(new_X)
+        return self.z
+
+    def GetZPrimeValues(self):
+        return map(self.LogisticPrime, self.z)
 
     def CostFunction(self, X, Y):
         yhat = self.Forward(X)
         J = 0.5*sum((Y-yhat)**2)
         return J
+
+    def TransposeWeights(self):
+        return map(np.transpose, self.weights)
+    
+    def CreateWeightZPairs(self):
+        
+        
+
+#    def CalculateErrors(self, X, Y):
+#        initial_delta = Y - self.Forward(X)
+#        deltas = []
+#        for i in self.HiddenLayers:
+#            for i in 
+        
+
+a = NeuralNetwork(2, 1, 3, 3)
